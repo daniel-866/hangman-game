ec70612ad04ce19b2ea0d9e5c931e87feff39b81
Daniel <daniel.laufer@outlook.com>
2023-01-17 11:30:25 +0200

    Code refactoring, new functions, infinite game loop with exit options

--- hangman.py
+++ hangman.py
@@ -1,18 +1,22 @@
 from random import choice
 
-# tries = 6
+words = []
+# There will be multiple files, different word groups
 filename = "words.txt"
 correct_letters = []
 wrong_letters = []
+guessed_words = []
 
-# 3: add a list of used words, to not repeat
+# Opens the words.txt and populates 'words' list.
+with open(filename, "r") as f:
+    word_list = f.readlines()
+    for i in word_list:
+        words.append(i.lower().strip("\n"))
 
 
-def choose_word(filename):
-    """Opens the words.txt and chooses a random word for the game."""
-    with open(filename, "r") as f:
-        word_list = f.readlines()
-        chosen_word = choice(word_list).lower()
+def choose_word(word_group):
+    """Chooses a random word for the game, that wasn't guessed correctly yet."""
+    chosen_word = choice(words)
     return chosen_word
 
 
@@ -30,27 +34,44 @@ def check_guess(chosen_word, guess):
 
 def set_difficulty():
     """Sets game difficulty, Easy, Normal, Hard"""
-    difficulty = input("Choose game difficulty:\n1)Easy\n2)Normal\n3)Hard\n> ").lower()
-    if difficulty == "1" or difficulty == "e" or difficulty == "easy":
-        tries = 10
-    elif difficulty == "2" or difficulty == "n" or difficulty == "normal":
-        tries = 6
-    elif difficulty == "3" or difficulty == "h" or difficulty == "hard":
-        tries = 6
-    return tries
+    while True:
+        difficulty = input(
+            "Choose game difficulty:\n1)Easy\n2)Normal\n3)Hard\n> "
+        ).lower()
+        if difficulty == "quit":
+            exit()
+        elif difficulty == "1":
+            tries = 10
+            return tries
+        elif difficulty == "2":
+            tries = 6
+            return tries
+        elif difficulty == "3":
+            tries = 4
+            return tries
+        else:
+            print("Enter a valid option.")
+            continue
 
 
-# Randomly selects a word from word_list
-chosen_word = choose_word(filename)
-
+# Game starts
 while True:
+    print("Enter 'quit' at any time to end the game.")
     # Set difficulty level (amount of tries)
     tries = set_difficulty()
-    # Round loop
+    # Reset tried letters
+    wrong_letters.clear()
+    correct_letters.clear()
+    # Randomly selects a word from word_list
+    chosen_word = choose_word(filename)
+
+    # Start of round
     while tries > 0:
         # Checks if entire word had already been entered before tries ran out.
         if len(correct_letters) == len(chosen_word):
-            print("You win!")
+            print("\nYou win!\n")
+            # guessed_words.append(chosen_word)
+            words.remove(chosen_word)
             break
         elif not len(correct_letters) and not len(wrong_letters):
             check_guess(chosen_word, "")
@@ -58,13 +79,15 @@ while True:
         print(f"\nTries left: {tries}")
         # Assign user input to variable guess
         guess = input("Guess a letter: ").lower()
+        if guess == "quit":
+            exit()
         if len(guess) > 1 or guess.isalpha() != True:
             print("ONLY ONE LETTER IS ALLOWED!")
             continue
 
         if guess in correct_letters or guess in wrong_letters:
             print("You already used this letter.")
-            check_guess(chosen_word, guess)
+            check_guess(chosen_word, "")
             continue
 
         if guess not in chosen_word:
